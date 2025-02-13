946cc91f2b3ec768f6afc88c40fa8c74ce677ed1
Yan Bisyukov <yanbisyukov@gmail.com>
2025-02-13 05:17:10 +0300

    add NumberServiceImplTest

---
+++ src/test/java/org/example/numbergenerateservice/service/impl/NumberServiceImplTest.java
@@ -0,0 +1,35 @@
+package org.example.numbergenerateservice.service.impl;
+
+import org.example.numbergenerateservice.model.GeneratedNumberEntity;
+import org.example.numbergenerateservice.repository.NumberRepository;
+import org.example.numbergenerateservice.service.NumberService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class NumberServiceImplTest {
+    @Mock
+    private NumberRepository repository;
+    @InjectMocks
+    private NumberService service;
+
+    @Test
+    void testGenerateUniqueNumber() {
+        GeneratedNumberEntity mockEntity = new GeneratedNumberEntity("1", "ABCDE20241212", LocalDateTime.now());
+        when(repository.save(any())).thenReturn(mockEntity);
+
+        GeneratedNumberEntity result = service.generate();
+        assertNotNull(result);
+        assertEquals(5, result.getFullNumber().substring(0, 5).length());
+    }
+}
